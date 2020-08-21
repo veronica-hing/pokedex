dc6dc087bbf28cc48b8f62c42a792e33089659c4
Veronica Hing <veronica.m.hing@gmail.com>
2020-08-21 11:16:39 -0700

    Updates

---
+++ precache-manifest.e70ce8f2ff04d0e59c97b10b30ec3397.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6140d0e93852d8e67ca0940064194962",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0ea447a5f0d85c852112",
+    "url": "/pokedex/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "936d4bed665efb0f5556",
+    "url": "/pokedex/static/css/main.398edfdb.chunk.css"
+  },
+  {
+    "revision": "0ea447a5f0d85c852112",
+    "url": "/pokedex/static/js/2.a5ccc1ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.a5ccc1ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936d4bed665efb0f5556",
+    "url": "/pokedex/static/js/main.2bc593f2.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "72b798aa2ef94623ea984987fd673788",
+    "url": "/pokedex/static/media/spinner.72b798aa.gif"
+  }
+]);
